59f72e3c3926ac54dd80fd34afdfa512c293bf07
Adam Birdsall <adam.birdsall4@gmail.com>
2017-07-17 20:07:07 -0700

    second commit

--- SmartDimmer/app/src/main/java/com/adambirdsall/smartdimmer/DiscoveryActivity.java
+++ SmartDimmer/app/src/main/java/com/adambirdsall/smartdimmer/DiscoveryActivity.java
@@ -3,11 +3,11 @@ package com.adambirdsall.smartdimmer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class DiscoverActivity extends AppCompatActivity {
+public class DiscoveryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_discover);
+        setContentView(R.layout.activity_discovery);
     }
 }
